a1887c2b42c170a14c58e2d323653c288a26d657
Humaid Khan <humaidk2@gmail.com>
2019-07-12 14:46:06 +0400

    import library and test functions

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request     
-
+from mapColoring.colorAssignment import colorAssignment
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/color")
+def home():
+	ca = colorAssignment()
+	ca.formatInput()
+    return ca.runModule()
 
 if __name__ == "__main__":
     app.run(debug=True)
