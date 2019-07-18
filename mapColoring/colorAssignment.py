fa0be2f6ca583c95214205726e5e1aa026385411
Humaid Khan <humaidk2@gmail.com>
2019-07-18 17:25:02 +0400

    Change format from dictionary to list to guarantee order

--- mapColoring/colorAssignment.py
+++ mapColoring/colorAssignment.py
@@ -37,7 +37,7 @@ class colorAssignment:
                             Neighbors = Neighbors + "," + elem["name"]
                 self.neighbors.append(Neighbors)
                 self.alphaCode.append(item["alpha2Code"])
-            self.countries = {}
+            self.countries = []
         else:
             print('An error has occurred.')
     
@@ -45,14 +45,19 @@ class colorAssignment:
 #   countries = {"india": { "neighbors": ["pakistan", "bangladesh", "nepal"], "options": ["red", "green", "blue"], "color": 'orange', "isAssigned": True}, "pakistan": {"neighbors": ["india", "bla"], "options": [ "green", "blue"], "isAssigned": False}}
     def formatInput(self, numOfCOlors):
         for idx,country in enumerate(self.countriesList):
-            self.countries[country] = {}
+            currCountry = {}
+            currCountry["name"] = country
             neighborList = str(self.neighbors[idx]).split(",")
             del neighborList[0]
-            self.countries[country]["neighbors"]= neighborList
-            self.countries[country]["options"] = [i for i in range(int(numOfCOlors))]
-            self.countries[country]["assigned"] = False
+            currCountry["neighbors"]= neighborList
+            currCountry["options"] = [i for i in range(int(numOfCOlors))]
+            currCountry["assigned"] = False
+            self.countries.append(currCountry)
 
-
+    def searchForCountry(self, countryName):
+        for idx, country in enumerate(self.countries):
+            if(country["name"] == countryName):
+                return idx
 # return false(try anothe color) if theres any conflicts in the assigned colors
 # return false(try anothe color)  if any options are empty(no option available)(conflict)
 # return true if all colors have successfully assigned
@@ -66,38 +71,38 @@ class colorAssignment:
         numOfAssigned = 0
         for country in countries:
             # check for conflicting options for assigned countries and their neghbours
-            if countries[country]["assigned"] == True:
-                for neighbor in countries[country]["neighbors"]:
-                    if countries[neighbor]["assigned"] == True:
-                        if countries[neighbor]["color"] == countries[country]["color"]:
+            if country["assigned"] == True:
+                for neighbor in country["neighbors"]:
+                    if countries[self.searchForCountry(neighbor)]["assigned"] == True:
+                        if countries[self.searchForCountry(neighbor)]["color"] == country["color"]:
                             return False
                 numOfAssigned += 1
             # if countries no options are available for a country return its not possible 
             else: 
-                if len(countries[country]["options"]) == 0:
+                if len(country["options"]) == 0:
                     return False
         # check if all color options have been assigned return that we're done
         if numOfAssigned == len(countries):
             return True
         else:
             # select next unassigned country and assign it
-            for country in countries:
-                if countries[country]["assigned"] == False:
-                    currCountry = country
-                    countries[currCountry]["assigned"] = True
+            for countryIndex, country in enumerate(countries):
+                if country["assigned"] == False:
+                    currCountryIndex = countryIndex
+                    country["assigned"] = True
                     break
             # for every possible color option for this unassigned country color
-            for option in countries[currCountry]["options"]:
+            for option in countries[currCountryIndex]["options"]:
                 # used for backtracking to store any options we delete
                 deletedOptions = []
                 # set the color to the current option
-                countries[currCountry]["color"] = option
+                countries[currCountryIndex]["color"] = option
                 # remove all the options from the neighbors and 
                 # store the removed neighbor in deletedOptions
-                for neighbor in countries[currCountry]["neighbors"]:
-                    if countries[currCountry]["color"] in countries[neighbor]["options"]:
-                        deletedOptions.append(neighbor)
-                        countries[neighbor]["options"].remove(option)
+                for neighbor in countries[currCountryIndex]["neighbors"]:
+                    if countries[currCountryIndex]["color"] in countries[self.searchForCountry(neighbor)]["options"]:
+                        deletedOptions.append(self.searchForCountry(neighbor))
+                        countries[self.searchForCountry(neighbor)]["options"].remove(option)
                         self.numOfNodesDeleted += 1
                 # recurse to the next variable
                 checkIfFinished = self.recursive(countries)
@@ -110,10 +115,10 @@ class colorAssignment:
                 # deleted the current color
                 else:
                     for deletedOption in deletedOptions:
-                        countries[deletedOption]["options"].append(countries[currCountry]["color"])
+                        countries[deletedOption]["options"].append(countries[currCountryIndex]["color"])
                 self.numOfTimesBackTracking += 1
-            countries[currCountry]["assigned"] = False
-            del countries[currCountry]["color"]
+            countries[currCountryIndex]["assigned"] = False
+            del countries[currCountryIndex]["color"]
             return False
 
     def runModule(self):
@@ -122,24 +127,24 @@ class colorAssignment:
         if isItPossible == True:
             finalOutput = []
             for country in self.countries:
-                if(self.countries[country]["color"] == 0):
-                    self.countries[country]["color"] = "red"
-                elif(self.countries[country]["color"] == 1):
-                    self.countries[country]["color"] = "blue"
-                elif(self.countries[country]["color"] == 2):
-                    self.countries[country]["color"] = "green"
-                elif(self.countries[country]["color"] == 3):
-                    self.countries[country]["color"] = "purple"
-                elif(self.countries[country]["color"] == 4):
-                    self.countries[country]["color"] = "yellow"
-                elif(self.countries[country]["color"] == 5):
-                    self.countries[country]["color"] = "pink"
-                elif(self.countries[country]["color"] == 6):
-                    self.countries[country]["color"] = "brown"
-                elif(self.countries[country]["color"] == 7):
-                    self.countries[country]["color"] = "orange"
-                self.colors.append(self.countries[country]["color"])
-                finalOutput.append([country, self.countries[country]["neighbors"], self.countries[country]["assigned"], self.countries[country]["color"]])
+                if(country["color"] == 0):
+                    country["color"] = "red"
+                elif(country["color"] == 1):
+                    country["color"] = "blue"
+                elif(country["color"] == 2):
+                    country["color"] = "green"
+                elif(country["color"] == 3):
+                    country["color"] = "purple"
+                elif(country["color"] == 4):
+                    country["color"] = "yellow"
+                elif(country["color"] == 5):
+                    country["color"] = "pink"
+                elif(country["color"] == 6):
+                    country["color"] = "brown"
+                elif(country["color"] == 7):
+                    country["color"] = "orange"
+                self.colors.append(country["color"])
+                finalOutput.append([country, country["neighbors"], country["assigned"], country["color"]])
                 # df = pd.DataFrame(finalOutput, columns=['country','neighbors', 'assigned', 'color'])
             return {
                 'countries': self.countriesList,
